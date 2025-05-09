bacfb2e41f58852cc730819c1519f149aa7b5c27
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-05-09 20:49:53 +0700

    fixed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -26,6 +26,27 @@ function Navbar() {
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                             <span className="navbar-toggler-icon"></span>
                         </button>
+                
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                        {navigationItems.map((item, idx) => (
+                            <li className="nav-item" key={idx}>
+                            <a 
+                                className={`nav-link px-3 ${item.active ? 'active fw-bold' : ''}`} 
+                                href="#"
+                                onClick={() => setActiveTab(item.name.toLowerCase())}
+                            >
+                                {item.name}
+                            </a>
+                            </li>
+                        ))}
+                        </ul>
+                        <div className="ms-3">
+                        <button className="btn btn-outline-secondary">
+                            Call Center
+                        </button>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </div>
